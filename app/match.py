5b865d7a2eaf229540f08db81acc81af02a98cd3
lupineka <maria_g_ah19@hotmail.com>
2014-09-16 03:02:32 -0500

    Cambios implementados

--- app/match.py
+++ app/match.py
@@ -1,6 +1,67 @@
 # -*- coding: utf-8 -*-
 class Match:
-    def __init__(self,player1,player2,pacted_sets):
+
+    def __init__(self, player1, player2, pacted_sets):
+        self.contP1 = 0
+        self.contP2 = 0
+        self.ganadores = []
+        self.scoreP1 = []
+        self.scoreP2 = []
         self.p1 = player1
         self.p2 = player2
         self.pacted_sets = pacted_sets
+
+    def score(self):
+        if(len(self.ganadores) == 0):
+            result = self.p1 + " plays with " + self.p2 + " | 0-0"
+            return result
+        else:
+            if(self.contP1 > self.contP2):
+                result = self.p1 + " defeated " + \
+                    self.p2 + " | " + self.results(self.p1)
+                return result
+            elif(self.contP2 > self.contP1):
+                result = self.p2 + " defeated " + \
+                    self.p1 + " | " + self.results(self.p2)
+                return result
+
+    def results(self, p):
+        if(p == self.p1):
+            if(len(self.ganadores) == 2):
+                scores = str(self.scoreP1[
+                             0]) + "-" + str(self.scoreP2[0]) + ", " + str(self.scoreP1[1]) + "-" + str(self.scoreP2[1])
+                return scores
+            if(len(self.ganadores) == 3):
+                scores = str(self.scoreP1[0]) + "-" + str(self.scoreP2[0]) + ", " + str(self.scoreP1[
+                    1]) + "-" + str(self.scoreP2[1]) + ", " + str(self.scoreP1[2]) + "-" + str(self.scoreP2[2])
+                return scores
+            if(len(self.ganadores) == 5):
+                scores = str(self.scoreP1[0]) + "-" + str(self.scoreP2[0]) + ", " + str(self.scoreP1[1]) + "-" + str(self.scoreP2[1]) + ", " + str(self.scoreP1[
+                    2]) + "-" + str(self.scoreP2[2]) + ", " + str(self.scoreP1[3]) + "-" + str(self.scoreP2[3]) + ", " + str(self.scoreP1[4]) + "-" + str(self.scoreP2[4])
+                return scores
+        elif(p == self.p2):
+            if(len(self.ganadores) == 2):
+                scores = str(self.scoreP2[
+                             0]) + "-" + str(self.scoreP1[0]) + ", " + str(self.scoreP2[1]) + "-" + str(self.scoreP1[1])
+                return scores
+            if(len(self.ganadores) == 3):
+                scores = str(self.scoreP2[0]) + "-" + str(self.scoreP1[0]) + ", " + str(self.scoreP2[
+                    1]) + "-" + str(self.scoreP1[1]) + ", " + str(self.scoreP2[2]) + "-" + str(self.scoreP1[2])
+                return scores
+            if(len(self.ganadores) == 5):
+                scores = str(self.scoreP2[0]) + "-" + str(self.scoreP1[0]) + ", " + str(self.scoreP2[1]) + "-" + str(self.scoreP1[1]) + ", " + str(self.scoreP2[
+                    2]) + "-" + str(self.scoreP1[2]) + ", " + str(self.scoreP2[3]) + "-" + str(self.scoreP1[3]) + ", " + str(self.scoreP2[4]) + "-" + str(self.scoreP1[4])
+                return scores
+
+    def asignar(self, p, num_set, n1, n2):
+        if(p == self.p1):
+            self.contP1 = self.contP1 + 1
+            self.ganadores = self.ganadores + [p]
+            self.scoreP1 = self.scoreP1 + [n1]
+            self.scoreP2 = self.scoreP2 + [n2]
+
+        elif(p == self.p2):
+            self.contP2 = self.contP2 + 1
+            self.ganadores = self.ganadores + [p]
+            self.scoreP2 = self.scoreP2 + [n1]
+            self.scoreP1 = self.scoreP1 + [n2]
--- tests/features/steps.py
+++ tests/features/steps.py
@@ -2,10 +2,9 @@
 from lettuce import *
 import app.match as m
 
-
 @step(u'Given: "([^"]*)" and "([^"]*)" start a match to "([^"]*)" sets')
-def given_group1_and_group2_start_a_match_to_group3_sets(step, group1, group2, group3):
-    world.match = m.Match(group1,group2,group3)
+def given_p1_and_p2_start_a_match_to_num_sets(step, p1, p2, num):
+    world.match = m.Match(p1,p2,num)
 
 
 @step(u'When: "([^"]*)" and "([^"]*)" start a match to "([^"]*)" sets')
@@ -14,22 +13,22 @@ def when_group1_and_group2_start_a_match_to_group3_sets(step, group1, group2, gr
 
 
 @step(u'Then: I see score: "([^"]*)"')
-def then_i_see_score_group1(step, group1):
-    assert False, 'This step must be implemented'
+def then_i_see_score_group1(step, score):
+    assert world.match.score() == score, "Got %s" % m.score()
 
 
 @step(u'When: "([^"]*)" won the "([^"]*)" set "([^"]*)"-"([^"]*)"')
 def when_group1_won_the_group2_set_group3_group4(step, group1, group2, group3, group4):
-    assert False, 'This step must be implemented'
+    world.match.asignar(group1, group2, group3, group4)
 
 
 @step(u'And: "([^"]*)" won the "([^"]*)" set "([^"]*)"-"([^"]*)"')
 def and_group1_won_the_group2_set_group3_group4(step, group1, group2, group3, group4):
-    assert False, 'This step must be implemented'
+    world.match.asignar(group1, group2, group3, group4)
 
 
 @step(u'Then: The match score is: "([^"]*)"')
 def then_the_match_score_is_group1(step, group1):
-    assert False, 'This step must be implemented'
+    assert world.match.score() == group1, "Got %s" % m.score()
 
 
